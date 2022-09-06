f62f465a6f85834a49a59f6c933cbf3d470fd01a
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2022-09-06 17:51:38 -0300

    Aula 1

---
+++ js/aula1.js
@@ -0,0 +1,5 @@
+alert('Primeira aula em JS!');
+
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>";
+
+document.write("Valor arredondado: " + parseFloat(1.516).toFixed(2)); // converte valor em float e arredonda para duas casas decimais
