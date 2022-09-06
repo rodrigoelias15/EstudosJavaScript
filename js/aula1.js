6c409ffb26942ad3d940da9163243b0d37e8458e
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2022-09-06 18:02:36 -0300

    Alteração aula 1

--- js/aula1.js
+++ js/aula1.js
@@ -1,5 +1,5 @@
 alert('Primeira aula em JS!');
 
-document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>";
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>"; // adiciona texto à tag com id "texto"
 
-document.write("Valor arredondado: " + parseFloat(1.516).toFixed(2)); // converte valor em float e arredonda para duas casas decimais
+document.write("Valor arredondado: " + parseFloat(1.516).toFixed(2)); // escreve no documento HTML onde a tag script se encontra e converte valor em float e o arredonda para duas casas decimais
