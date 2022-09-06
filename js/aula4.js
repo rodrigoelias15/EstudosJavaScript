9277473c9255254258fd12748947aef743bf1405
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2022-09-06 18:18:20 -0300

    Aula 4

---
+++ js/aula4.js
@@ -0,0 +1,9 @@
+/* Funções */
+
+function soma(valor1, valor2) {
+    return "Soma: " + valor1 + valor2;
+}
+
+var varSoma;
+varSoma = soma(5,2);
+document.getElementById("texto").innerHTML = varSoma;
